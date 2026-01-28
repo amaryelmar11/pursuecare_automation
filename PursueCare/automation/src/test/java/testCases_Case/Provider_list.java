be3826e47defa10893f1b364574351853d703e6a
skhedkar-automationTester <skhedkar@tech.saisystems.com>
2026-01-28 01:51:33 -0500

    01_28_2026

--- PursueCare/automation/src/test/java/testCases_Case/Provider_list.java
+++ PursueCare/automation/src/test/java/testCases_Case/Provider_list.java
@@ -3,6 +3,8 @@ package testCases_Case;
 
 import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,10 +12,14 @@ import org.testng.annotations.Test;
 import BaseClass.Baseclass;
 import PageObjectClass.DashBoardPage;
 import PageObjectClass.LoginPage;
+import PageObjectClass.Patient_Creation_Page;
+import PageObjectClass.Provider_List_Page;
+import PageObjectClassCM.Provider_list_CM_Page;
 import utilities.TableSortUtils;
 import PageObjectClass.CM_Role;
 import org.testng.annotations.BeforeClass;
 import BasePage.Common_Utils;
+import BasePage.RandomData;
 
 
 
@@ -26,6 +32,13 @@ public class Provider_list extends Baseclass{
     private Common_Utils cu;
     private CM_Role pc;
     private TableSortUtils ts;
+    private Provider_List_Page pl;
+    private Provider_list_CM_Page plc;
+    private String providerEmail;
+    private Patient_Creation_Page pcc;
+    private String firstName;
+    private String lastName;
+
 
     @BeforeClass
     public void initPages() {
@@ -34,6 +47,12 @@ public class Provider_list extends Baseclass{
         cu = new Common_Utils(driver);
         pc = new CM_Role (driver);
         ts = new TableSortUtils();
+        pl = new Provider_List_Page(driver);
+        plc = new Provider_list_CM_Page(driver);
+        firstName = RandomData.getFirstName();
+        lastName =  RandomData.getLastName();
+        providerEmail = firstName + "@pursuecare.com";
+        pcc = new Patient_Creation_Page(driver);
   
 
 }
@@ -92,5 +111,51 @@ public class Provider_list extends Baseclass{
             List<WebElement> column1 = cu.getColumnElements(1);
             boolean status1 = TableSortUtils.isColumnSortedUniversal(column1, "desc");
             Assert.assertTrue(status1);
+
+            cu.checkColumnSorting(4);
+            cu.checkColumnSorting(5);
+            cu.checkColumnSorting(6);
+            cu.checkColumnSorting(7);
+            cu.checkColumnSorting(8);
+    }
+
+    @Test(priority = 2)
+    public void createProvider() throws InterruptedException 
+    {
+        cu.click(pl.ClickAddItemBtn);
+        cu.enterText(plc.firstname, firstName);
+        cu.enterText(plc.lastname, lastName);
+        cu.enterText(plc.ProviderEmail, providerEmail);
+        cu.click(pcc.add_pat_Timezone);
+        cu.click(pcc.timezone_EST);
+        cu.click(pcc.add_patient_DateOfBirth);
+        cu.enterText(pcc.add_patient_DateOfBirth, cu.getRandomDOB());
+        cu.enterText(plc.EnterNumber, RandomData.getPhone());
+        cu.click(plc.SelectSpeciality);
+        cu.click(plc.pursuecareRXSpeciality);
+        cu.click(plc.PsychMeditationManager);
+        cu.click(plc.TherapyCounseling);
+        cu.click(plc.MATDescriber);
+        cu.click(plc.AdmissionCouseller);
+        driver.findElement(By.tagName("body")).sendKeys(Keys.ESCAPE);
+        cu.click(pl.ClickEmployeeType);
+        cu.click(plc.SWEmpType);
+        driver.findElement(By.tagName("body")).sendKeys(Keys.ESCAPE);
+        cu.click(pc.Save_Patient_Info);
+        Thread.sleep(5000);
+       
+    }
+
+    @Test(priority = 3)
+    public void verifyProviderCreated() throws InterruptedException
+    {
+        cu.enterText(plc.SearchFilterBtn, providerEmail);
+        String createdProviderName = cu.getElementText(plc.GetCreatedProviderName).trim();
+        Assert.assertEquals(createdProviderName, firstName + " " + lastName);
+    }
+
+    public void ResetPasswordForProvider() throws InterruptedException
+    {
+        
     }
 }
